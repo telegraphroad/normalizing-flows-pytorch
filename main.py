23e0197187d5940507e0e767bfcd02655dc32375
Saba Amiri <saba.amiri@outlook.com>
2021-12-06 17:58:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -997,8 +997,8 @@ def main(cfg):
 
     # setup train/eval model
     #model = Model(dims=dataset.dims, datatype=dataset.dtype, cfg=cfg, bd_family = 'ggd', variable_bd = True, loc = 0., scale = 1., p = 0.1, dim = cfg.run.dim)
-    model = Model(dims=dataset.dims, datatype=dataset.dtype, cfg=cfg, bd_family = 'mvn', variable_bd = True, mu = 0., cov = 1.)
-    #model = Model(dims=dataset.dims, datatype=dataset.dtype, cfg=cfg, bd_family = 'mvggd', variable_bd = True, loc = 0., scale = 1., p = 0.1, dw=1., dim = cfg.run.dim)
+    #model = Model(dims=dataset.dims, datatype=dataset.dtype, cfg=cfg, bd_family = 'mvn', variable_bd = True, mu = 0., cov = 1.)
+    model = Model(dims=dataset.dims, datatype=dataset.dtype, cfg=cfg, bd_family = 'mvggd', variable_bd = True, loc = 0., scale = 1., p = 0.1, dw=1., dim = cfg.run.dim)
 
     # summary writer
     writer = SummaryWriter('./')
