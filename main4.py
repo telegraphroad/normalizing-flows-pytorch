082a2b62d8ecd24f8cdc4b47f394ed11dfcfbdd8
Saba Amiri <saba.amiri@outlook.com>
2021-12-14 13:10:31 +0100

    Update main4.py

--- main4.py
+++ main4.py
@@ -1288,7 +1288,7 @@ def main(cfg):
                                 ckpt_file = prefix + 'latest.pth'
                                 model.save_ckpt(step, ckpt_file)
                         x = torch.FloatTensor(gennorm(beta=vdbeta).rvs(size=[20000,2])).to(device)
-                        print('HHHHHHHHHHHHHHHHHHH',get_tail_index(x)
+                        print('HHHHHHHHHHHHHHHHHHH',get_tail_index(x))
                         px = np.mean(np.exp(gennorm(beta=vdbeta).logpdf(x.detach().cpu().numpy())),axis=1)
                         qx = model.log_py(x)
                         print('PX',px)
