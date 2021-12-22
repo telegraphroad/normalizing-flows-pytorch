5d14c9d7c0d480bdf5b0981e4ccf8218c2b762c8
Saba Amiri <saba.amiri@outlook.com>
2021-12-22 12:51:58 +0100

    Update coupling.py

--- flows/coupling.py
+++ flows/coupling.py
@@ -31,7 +31,7 @@ class AbstractCoupling(nn.Module):
                             (masking, str(dims)))
 
     def forward(self, z, log_df_dz):
-        print('coupling33,z',z.shape)
+        #print('coupling33,z',z.shape)
         z0, z1 = self.squeeze(z)
         z0, z1, log_df_dz = self._transform(z0, z1, log_df_dz)
         z = self.unsqueeze(z0, z1)
