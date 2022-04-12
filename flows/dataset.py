700dfbb9340ca08c09e712401e671585339ea3b8
Saba Amiri <saba.amiri@outlook.com>
2022-04-12 18:04:17 +0200

    Update dataset.py

--- flows/dataset.py
+++ flows/dataset.py
@@ -162,7 +162,7 @@ class FlowDataLoader(object):
 
         elif self.name == 'funnel':
             self.dset = _sample_funnel(N_DATASET_SIZE)
-            self.dims = 2
+            self.dims = (2,)
             self.dtype = '2d'
             self.bdist = NealFunnelDist()
 
